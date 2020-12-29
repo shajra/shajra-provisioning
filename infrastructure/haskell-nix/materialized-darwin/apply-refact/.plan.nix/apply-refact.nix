40f4f1504bb6075cfe1c9ef1afb9e16228f1448b
Sukant Hajra <rrudbskr5g@snkmail.com>
2020-12-29 17:47:27 -0600

    Haskell.nix materialization for Darwin

---
+++ infrastructure/haskell-nix/materialized-darwin/apply-refact/.plan.nix/apply-refact.nix
@@ -0,0 +1,135 @@
+{ system
+  , compiler
+  , flags
+  , pkgs
+  , hsPkgs
+  , pkgconfPkgs
+  , errorHandler
+  , config
+  , ... }:
+  {
+    flags = {};
+    package = {
+      specVersion = "1.10";
+      identifier = { name = "apply-refact"; version = "0.8.2.1"; };
+      license = "BSD-3-Clause";
+      copyright = "";
+      maintainer = "matthewtpickering@gmail.com";
+      author = "Matthew Pickering";
+      homepage = "";
+      url = "";
+      synopsis = "Perform refactorings specified by the refact library.";
+      description = "Perform refactorings specified by the refact library. It is primarily used with HLint's --refactor flag.";
+      buildType = "Simple";
+      isLocal = true;
+      detailLevel = "FullDetails";
+      licenseFiles = [ "LICENSE" ];
+      dataDir = "";
+      dataFiles = [];
+      extraSrcFiles = [
+        "CHANGELOG"
+        "README.md"
+        "tests/examples/*.hs"
+        "tests/examples/*.hs.refact"
+        "tests/examples/*.hs.expected"
+        ];
+      extraTmpFiles = [];
+      extraDocFiles = [];
+      };
+    components = {
+      "library" = {
+        depends = [
+          (hsPkgs."base" or (errorHandler.buildDepError "base"))
+          (hsPkgs."refact" or (errorHandler.buildDepError "refact"))
+          (hsPkgs."ghc-exactprint" or (errorHandler.buildDepError "ghc-exactprint"))
+          (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))
+          (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+          (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+          (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+          (hsPkgs."process" or (errorHandler.buildDepError "process"))
+          (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+          (hsPkgs."filemanip" or (errorHandler.buildDepError "filemanip"))
+          (hsPkgs."unix-compat" or (errorHandler.buildDepError "unix-compat"))
+          (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+          ];
+        buildable = true;
+        modules = [
+          "Refact/Internal"
+          "Refact/Utils"
+          "Refact/Apply"
+          "Refact/Fixity"
+          ];
+        hsSourceDirs = [ "src" ];
+        };
+      exes = {
+        "refactor" = {
+          depends = [
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."refact" or (errorHandler.buildDepError "refact"))
+            (hsPkgs."ghc-exactprint" or (errorHandler.buildDepError "ghc-exactprint"))
+            (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))
+            (hsPkgs."ghc-boot-th" or (errorHandler.buildDepError "ghc-boot-th"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."filemanip" or (errorHandler.buildDepError "filemanip"))
+            (hsPkgs."unix-compat" or (errorHandler.buildDepError "unix-compat"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = true;
+          modules = [
+            "Paths_apply_refact"
+            "Refact/Apply"
+            "Refact/Fixity"
+            "Refact/Internal"
+            "Refact/Utils"
+            "Refact/Run"
+            ];
+          hsSourceDirs = [ "src" ];
+          mainPath = [ "Main.hs" ];
+          };
+        };
+      tests = {
+        "test" = {
+          depends = [
+            (hsPkgs."tasty" or (errorHandler.buildDepError "tasty"))
+            (hsPkgs."tasty-golden" or (errorHandler.buildDepError "tasty-golden"))
+            (hsPkgs."tasty-expected-failure" or (errorHandler.buildDepError "tasty-expected-failure"))
+            (hsPkgs."base" or (errorHandler.buildDepError "base"))
+            (hsPkgs."refact" or (errorHandler.buildDepError "refact"))
+            (hsPkgs."ghc-exactprint" or (errorHandler.buildDepError "ghc-exactprint"))
+            (hsPkgs."ghc" or (errorHandler.buildDepError "ghc"))
+            (hsPkgs."ghc-boot-th" or (errorHandler.buildDepError "ghc-boot-th"))
+            (hsPkgs."containers" or (errorHandler.buildDepError "containers"))
+            (hsPkgs."extra" or (errorHandler.buildDepError "extra"))
+            (hsPkgs."syb" or (errorHandler.buildDepError "syb"))
+            (hsPkgs."process" or (errorHandler.buildDepError "process"))
+            (hsPkgs."directory" or (errorHandler.buildDepError "directory"))
+            (hsPkgs."optparse-applicative" or (errorHandler.buildDepError "optparse-applicative"))
+            (hsPkgs."filemanip" or (errorHandler.buildDepError "filemanip"))
+            (hsPkgs."unix-compat" or (errorHandler.buildDepError "unix-compat"))
+            (hsPkgs."filepath" or (errorHandler.buildDepError "filepath"))
+            (hsPkgs."silently" or (errorHandler.buildDepError "silently"))
+            (hsPkgs."transformers" or (errorHandler.buildDepError "transformers"))
+            ];
+          buildable = if compiler.isGhc && (compiler.version).lt "7.10"
+            then false
+            else true;
+          modules = [
+            "Paths_apply_refact"
+            "Refact/Run"
+            "Refact/Apply"
+            "Refact/Fixity"
+            "Refact/Internal"
+            "Refact/Utils"
+            ];
+          hsSourceDirs = [ "tests" "src" ];
+          mainPath = [ "Test.hs" ];
+          };
+        };
+      };
+    } // rec { src = (pkgs.lib).mkDefault ../.; }
