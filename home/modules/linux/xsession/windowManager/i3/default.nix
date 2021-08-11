571c880f8f0a2e6af17d754553f12fbe8549b281
Sukant Hajra <rrudbskr5g@snkmail.com>
2021-08-11 11:57:32 -0500

    home: linux: i3: improve startup experience

--- home/modules/linux/xsession/windowManager/i3/default.nix
+++ home/modules/linux/xsession/windowManager/i3/default.nix
@@ -2,9 +2,13 @@ config: pkgs:
 
 let
     alacritty-exe = "${config.programs.alacritty.package}/bin/alacritty";
+    autorandr-exe = "${pkgs.autorandr}/bin/autorandr";
+    i3msg-exe  = "${config.xsession.windowManager.i3.package}/bin/i3-msg";
     i3status-exe  = "${config.programs.i3status-rust.package}/bin/i3status-rs";
+    i3-conf = "${config.xdg.configHome}/i3status-rust/config-bottom.toml";
     i3status-conf = "${config.xdg.configHome}/i3status-rust/config-bottom.toml";
-    runCmd = cmd: { command = cmd; always = true; };
+    runAlways = cmd: { command = cmd; always = true; notification = false; };
+    runOnce = cmd: { command = cmd; always = false; notification = false; };
 in
 
 {
@@ -17,7 +21,11 @@ in
         keybindings = import ./keybindings.nix modifier config pkgs alacritty-exe;
         modes = import ./modes.nix modifier;
         modifier = "Mod4";
-        startup = [(runCmd ". ~/.xprofile; systemctl --user start graphical-session.target")];
+        startup = [
+            (runAlways ". ~/.xprofile; systemctl --user start graphical-session.target")
+            (runOnce "${i3msg-exe} workspace 1")
+            (runOnce "${autorandr-exe} --change --default home")
+        ];
         terminal = alacritty-exe;
     };
 }
