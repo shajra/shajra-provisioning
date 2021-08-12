71bce37bbe5425f6c95f87ce0d8a308f07d4e3cf
Sukant Hajra <rrudbskr5g@snkmail.com>
2021-08-11 23:41:48 -0500

    home: doom: moved some font settings from private config

--- home/modules/base/xdg/configFile/doom/config.el
+++ home/modules/base/xdg/configFile/doom/config.el
@@ -5,8 +5,13 @@
 
 (setq-default
  dante-methods '(alt-stack-project alt-cabal)
+ doom-big-font            (font-spec :family "SauceCodePro Nerd Font" :size 18.0)
+ doom-font                (font-spec :family "SauceCodePro Nerd Font" :size 11.0)
  doom-large-file-size-alist '(("/TAGS\\([.]local\\)?$". 50.0) ("." . 1.0))
+ doom-serif-font          (font-spec :family "Source Serif Pro" :size 11.0)
  doom-theme 'doom-solarized-light
+ doom-unicode-font        (font-spec :family "FreeSerif")  ; alternative to Symbola
+ doom-variable-pitch-font (font-spec :family "Source Serif Variable" :size 11.0)
  fancy-splash-image "~/.config/doom/snowman.png"
  fill-column 80
  +haskell-dante-xref-enable nil
@@ -21,6 +26,18 @@
 
 (add-to-list '+org-babel-mode-alist '(fish . shell))
 
+;; DESIGN: this is a hack to deal with the modeline getting too big.  See the
+;; code for ‘doom-modeline--font-height’ to get a glimpse at how this is just a
+;; hack on top of what is looks like already a hack.
+;;
+;; I believe that all the settings below in tandem should work fine across
+;; different computers and displays.
+;;
+(custom-set-faces!
+  '(mode-line :family "Source Serif Variable" :height 0.9)
+  '(mode-line-inactive :family "Source Serif Variable" :height 0.9))
+(advice-add #'doom-modeline--font-height :override #'(lambda () (progn 28)))
+
 
 ;; Function calls
 
@@ -31,6 +48,12 @@
 
 ;; Hooks
 
+;; DESIGN: font with Haskell ligatures, but restricted to Haskell code
+(add-hook! haskell-mode
+  (setq buffer-face-mode-face '(:family "Hasklug Nerd Font"))
+  (buffer-face-mode))
+
+;; DESIGN: maybe redundant with the default, but using etags with xref
 (after! xref
   (add-hook 'xref-backend-functions 'etags--xref-backend))
 
@@ -44,13 +67,6 @@
 (add-hook! org-mode :append
   (if (+org-agenda-files-includes? (buffer-name))
       (org-indent-mode)))
-
-;; DESIGN: keepin with Doom Emacs defaults.
-;;(add-hook! org-load :append
-;;  (setq-default
-;;   ;; DESIGN: Emacs default value is nil, which 2-space pads SRC blocks.
-;;   ;; Doom Emacs default is t, which puts no padding.
-;;   org-src-preserve-indentation nil))
 
 ;; DESIGN: Some configuration is more personal...
 (load! "config" "/home/tnks/.config/doom-private" t)
