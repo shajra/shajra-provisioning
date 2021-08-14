50bae29fa2e6b9cca1673cf775bee7df03bd5efd
Sukant Hajra <rrudbskr5g@snkmail.com>
2021-08-14 14:34:27 -0500

    home: hole: symlink for Moneydance

--- home/target/hole/default.nix
+++ home/target/hole/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
     imports = [ ../../modules/linux ];
@@ -6,7 +6,7 @@
     home.homeDirectory = "/home/tnks";
     home.username = "tnks";
 
-    home.file = import home/file;
+    home.file = import home/file config pkgs;
 
     programs.autorandr = import programs/autorandr pkgs.i3-dpi;
     programs.i3status-rust = import programs/i3status-rust pkgs;
--- home/target/hole/home/file/default.nix
+++ home/target/hole/home/file/default.nix
@@ -1,3 +1,11 @@
+config: pkgs:
+
+let
+    makeLink = link: pkgs.runCommand "makeLink" {} "ln -s ${link} $out";
+in
+
 {
     ".unison/default.prf".source = unison/default.prf;
+    ".moneydance/Documents/tnks.moneydance".source = makeLink
+        "${config.home.homeDirectory}/doc/shared/sensitive/finance/moneydance";
 }
