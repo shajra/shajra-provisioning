a935f8df858c66a232b94fbb15b89a46f667af5d
Sukant Hajra <rrudbskr5g@snkmail.com>
2021-08-27 19:58:49 -0500

    home: base: kitty: improving Solarized theming

--- home/modules/base/programs/kitty/default.nix
+++ home/modules/base/programs/kitty/default.nix
@@ -1,5 +1,11 @@
 pkgs:
 
+let
+    hintsOpen = type: "kitten hints --type=${type}"
+        + " --hints-text-color 'green' --hints-foreground-color white";
+    hintsInsert = type: "${hintsOpen type} --program -";
+in
+
 {
     enable = true;
     extraConfig = ''
@@ -8,11 +14,22 @@ pkgs:
     '';
     font.name = "SauceCodePro Nerd Font Mono";
     font.package = pkgs.nerdfonts;
+    keybindings = {
+        "ctrl+shift+e"         = hintsOpen   "url";
+        "ctrl+shift+p>f"       = hintsInsert "path";
+        "ctrl+shift+p>shift+f" = hintsOpen   "path";
+        "ctrl+shift+p>h"       = hintsInsert "hash";
+        "ctrl+shift+p>l"       = hintsInsert "line";
+        "ctrl+shift+p>n"       = hintsOpen   "linenum";
+        "ctrl+shift+p>w"       = hintsInsert "word";
+        "ctrl+shift+p>y"       = hintsOpen   "hyperlink";
+    };
     settings = {
         enable_audio_bell    = false;
         background           = "#fdf6e3";
         foreground           = "#657b83";
         cursor               = "#586e75";
+        cursor_text_color    = "background";
         selection_background = "#475b62";
         selection_foreground = "#eae3cb";
         color0               = "#073642";
@@ -31,5 +48,13 @@ pkgs:
         color14              = "#93a1a1";
         color7               = "#eee8d5";
         color15              = "#fdf6e3";
+        url_color            = "#2aa198";
+        mark1_foreground     = "#fdf6e3";
+        mark1_background     = "#859900";
+        mark2_foreground     = "#fdf6e3";
+        mark2_background     = "#268bd2";
+        mark3_foreground     = "#fdf6e3";
+        mark3_background     = "#cb4b16";
+        tab_bar_style        = "powerline";
     };
 }
