17078ff41fb7f1971f12325d0f7b0e35f1033473
Sukant Hajra <rrudbskr5g@snkmail.com>
2021-09-13 08:45:44 -0500

    HOME: LINUX: I3 color change

--- home/modules/linux/xsession/windowManager/i3/colors.nix
+++ home/modules/linux/xsession/windowManager/i3/colors.nix
@@ -10,6 +10,7 @@ let
     base00     = "#657b83";  # standard text
     base0      = "#839496";
     base1      = "#93a1a1";  # shadowed comments
+    base15     = "#c0c4bb";  # non-Solarized semitone
     base2      = "#eee8d5";  # background highlights
     base3      = "#fdf6e3";  # background
     yellow     = "#b58900";
@@ -21,16 +22,22 @@ let
     cyan       = "#2aa198";
     green      = "#859900";
 
+    focus_bg      = green;
+    focus_fg      = base3;
+    unfocus_bg    = base15;
+    unfocus_fg_em = base03;
+    unfocus_fg    = base01;
+
 in
 
 {
     # DESIGN: The "child border" is the prominent border.  The "border" is just
     # the very thin lines around the title.
     #
-    #                             border bground text   indicator cborder
-    urgent          = clientColor base01 base1   orange red       base1;
-    focused         = clientColor green  green   base3  cyan      green;
-    focusedInactive = clientColor base01 base1   base3  violet    base1;
-    unfocused       = clientColor base01 base1   base02 violet    base1;
-    placeholder     = clientColor base3  base3   base01 base01    base3;
+    #                             border   bground    text          indicator cborder
+    urgent          = clientColor base01   unfocus_bg magenta       violet    unfocus_bg;
+    focused         = clientColor focus_bg focus_bg   focus_fg      base00    focus_bg;
+    focusedInactive = clientColor base01   unfocus_bg unfocus_fg_em base1     unfocus_bg;
+    unfocused       = clientColor base01   unfocus_bg unfocus_fg    base1     unfocus_bg;
+    placeholder     = clientColor base3    base3      base01        base01    base3;
 }
