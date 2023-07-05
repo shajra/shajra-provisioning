7142881f4546b429e9eba1cbc2410ec4a550c287
Sukant Hajra <dev.sukant@hajra.xyz>
2023-07-05 12:47:57 -0500

    Update Doom packages

--- home/modules/base/tui/all/xdg/configFile/doom/static/modules/lang/haskell-extn/packages.el
+++ home/modules/base/tui/all/xdg/configFile/doom/static/modules/lang/haskell-extn/packages.el
@@ -6,11 +6,11 @@
 ;; DESIGN: match Doom
 (package! haskell-mode :pin "a34ccdc54be15043ff0d253c3c20087524255491")
 
-(when (modulep! +dante)
-  ;; DESIGN: target latest for both
-  (package! dante :pin "914d4f21252a66fe526abedebe24703bc73397d9")
-  (package! attrap :pin "2df105d0bd23a468e75fad0fed7d39013328526d"))
-
 (when (and (modulep! +lsp) (not (modulep! :tools lsp +eglot)))
   ;; DESIGN: match Doom
   (package! lsp-haskell :pin "3249cde75fb411f95fe173c222b848182fd0b752"))
+
+(when (modulep! +dante)
+  ;; DESIGN: target latest for both
+  (package! dante :pin "914d4f21252a66fe526abedebe24703bc73397d9")
+  (package! attrap :pin "1a0af0d210d5276f4d6c47ea53661c44437f9b12"))
