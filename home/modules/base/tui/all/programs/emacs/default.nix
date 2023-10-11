67f32e24772dae1f0665983152b2fb01107473fc
Sukant Hajra <rrudbskr5g@snkmail.com>
2023-10-11 16:43:32 -0500

    Put Mac on Emacs 29 as well

--- home/modules/base/tui/all/programs/emacs/default.nix
+++ home/modules/base/tui/all/programs/emacs/default.nix
@@ -1,8 +1,13 @@
-{
+pkgs: {
     enable = true;
+
     extraPackages = epkgs: with epkgs; [
         emacsql
         emacsql-sqlite
         vterm
     ];
+
+    # DESIGN: https://github.com/doomemacs/doomemacs#prerequisites
+    # using emacs29 over emacs-unstable to hit Nixpkgs cache
+    package = pkgs.emacs29;
 }
