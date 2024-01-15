d4b5ee18c68e997786ed113d3a41ecfb05f5f7b8
Sukant Hajra <rrudbskr5g@snkmail.com>
2024-01-15 16:21:33 -0600

    Linux: refactor i3status-rust

---
+++ home/modules/base/gui/linux/programs/i3status-rust/default.nix
@@ -0,0 +1,42 @@
+config: pkgs:
+
+let
+
+    format = f: x: pkgs.lib.colors.format "#%R%G%B" (f x);
+    id = x: x;
+    foregroundFor = config.theme.colors.nominal.foregroundFor;
+    colors = pkgs.lib.colors.transformColors (format id) config.theme.colors;
+    foreground = pkgs.lib.colors.transformColors (format foregroundFor) config.theme.colors;
+
+in
+
+{
+    enable = true;
+    bars = {
+        bottom = {
+            icons = "awesome6";
+            settings.theme = {
+                overrides = {
+                    idle_bg             = colors.semantic.unifying;
+                    idle_fg             = foreground.semantic.unifying;
+                    good_bg             = colors.semantic.good;
+                    good_fg             = foreground.semantic.good;
+                    info_bg             = colors.semantic.info;
+                    info_fg             = foreground.semantic.info;
+                    warning_bg          = colors.semantic.warning;
+                    warning_fg          = foreground.semantic.warning;
+                    critical_bg         = colors.semantic.urgent;
+                    critical_fg         = foreground.semantic.urgent;
+
+                    alternating_tint_bg = "#202020";  # lighten
+                    alternating_tint_fg = "#202020";  # lighten
+
+                    separator_bg        = "auto";
+                    separator_fg        = "auto";
+                    separator           = "";
+                    #separator           = "";
+                };
+            };
+        };
+    };
+}
