2dad32c0c15170dfcae75e9d47320dec2fb60934
Sukant Hajra <rrudbskr5g@snkmail.com>
2024-01-18 00:01:16 -0600

    Fix VM environment variables

--- home/target/shajra/home/file/default.nix
+++ home/target/shajra/home/file/default.nix
@@ -7,7 +7,8 @@ in
     ".chrome-remote-desktop-session".text = ''
         systemctl --user start --wait gpg-agent.service
         systemctl --user start --wait clipmenu.service
-        export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+        export XDG_RUNTIME_DIR="/run/user/$(id -u)"
+        export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
         export SSH_AUTH_SOCK="$("${gpg}/bin/gpgconf" --list-dirs agent-ssh-socket)"
         . ~/.xsession
     '';
