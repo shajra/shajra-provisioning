2412891e928c276497acd3124674efe10d642c48
Sukant Hajra <rrudbskr5g@snkmail.com>
2024-01-25 01:06:57 -0600

    Darwin: Homebrew cleanup

--- machines/bagel/homebrew/default.nix
+++ machines/bagel/homebrew/default.nix
@@ -1,7 +1,9 @@
 {
     enable = true;
     onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
     global = {
+        autoUpdate = true;
         brewfile = true;
     };
 
@@ -12,14 +14,51 @@
         "homebrew/cask-versions"
         "homebrew/services"
         "koekeishiya/formulae"
-        "FelixKratz/formulae"
+        "felixkratz/formulae"
     ];
 
     brews = [
-        "borders"
-        "sketchybar"
-        "skhd"
-        "yabai"
+        # REVISIT: Darwin WM with Homebrew versus Nix
+        #
+        # Borders returns borders which were removed in MacOS 14.
+        # Unfortunately, the latest MacOS SDK in Nixpkgs is still SDK 11.
+        # Nixpkgs has the latest Yabai, but patches it slightly to keep
+        # compilation with SDK 11. Both Sketchybar and Skhd are in Nixpkgs with
+        # no patching.  Consequently, Nix-Darwin supports management of
+        # Sketchybar, Skhd, and Yabai, but not Borders.
+        #
+        # Rather than split the installation of Darwin window management across
+        # both Homebrew and Nixpkgs, it seems easier to leave all these four
+        # packages with Homebrew.
+        #
+        # See https://github.com/NixOS/nixpkgs/issues/101229.
+        #
+        # NOTE: Remember that when using HEAD versions (sometimes needed for hot
+        # fixes), Home Manager won't force Homebrew to recompile a package if
+        # it's already installed.  To switch to/from HEAD, you'll have to
+        # manually remove the package before calling darwin-rebuild.  Home
+        # Manager is okay for initialization with Homebrew, but has notable
+        # limitations when updating.
+        #
+        # With Homebrew, you can see the version installed by looking at the
+        # filepath of the installation:
+        #
+        #     ❯ brew uninstall --force sketchybar
+        #     ❯ brew install sketchybar --HEAD
+
+        #     ❯ readlink -f "$(which sketchybar)"
+        #     /opt/homebrew/Cellar/sketchybar/HEAD-e2009ab/bin/sketchybar
+        #
+        #     ❯ brew uninstall --force sketchybar
+        #     ❯ brew install sketchybar
+        #
+        #     ❯ readlink -f "$(which sketchybar)"
+        #     /opt/homebrew/Cellar/sketchybar/2.19.6/bin/sketchybar
+        #
+        { name = "borders"; }
+        { name = "sketchybar"; args = ["HEAD"]; restart_service = "changed"; }
+        { name = "skhd"; }
+        { name = "yabai"; }
 
         # DESIGN: not needed for now
         #"kubernetes-cli"
@@ -41,7 +80,9 @@
 
     # DESIGN: homebrew.masApps seems a bit too hacky
 
-    # DESIGN: installed manually; haven't figured out with either Brew or Nix
+    # REVISIT: installed manually; haven't figured out with either Homebrew or
+    # Nix
+    #
     # /Applications/'BluOS Controller.app'
     # /Applications/'Chrome Remote Desktop Host Uninstaller.app'
     # /Applications/DDPM
@@ -58,10 +99,4 @@
     # /Applications/'TigerVNC Viewer 1.11.0.app'
     # /Applications/'Wacom Tablet.localized'
     # /Applications/WireGuard.app
-
-    # DESIGN: In case more cutting edge versions are needed
-    #extraConfig = ''
-    #    brew "koekeishiya/formulae/yabai", args: ["HEAD"], restart_service: :changed
-    #    brew "koekeishiya/formulae/skhd", args: ["HEAD"], restart_service: :changed
-    #'';
 }
