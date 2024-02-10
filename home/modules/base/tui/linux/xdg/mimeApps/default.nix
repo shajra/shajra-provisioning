a010e3ad92a7c0a78c60135ed7c2a4b4b7285878
Sukant Hajra <dev.sukant@hajra.xyz>
2024-02-10 01:56:31 -0600

    Linux: Browser tinkering

--- home/modules/base/tui/linux/xdg/mimeApps/default.nix
+++ home/modules/base/tui/linux/xdg/mimeApps/default.nix
@@ -4,17 +4,17 @@ rec {
     defaultApplications = {
         "application/pdf" = "org.pwmt.zathura.desktop";
 
-        "application/x-extension-html"  = "microsoft-edge-beta.desktop";
-        "application/x-extension-htm"   = "microsoft-edge-beta.desktop";
-        "application/x-extension-shtml" = "microsoft-edge-beta.desktop";
-        "application/x-extension-xht"   = "microsoft-edge-beta.desktop";
-        "application/x-extension-xhtml" = "microsoft-edge-beta.desktop";
-        "application/xhtml+xml"         = "microsoft-edge-beta.desktop";
-        "text/html"                     = "microsoft-edge-beta.desktop";
-        "x-scheme-handler/chrome"       = "microsoft-edge-beta.desktop";
-        "x-scheme-handler/http"         = "microsoft-edge-beta.desktop";
-        "x-scheme-handler/https"        = "microsoft-edge-beta.desktop";
-        "x-scheme-handler/mailto"       = "microsoft-edge-beta.desktop";
+        "application/x-extension-html"  = "microsoft-edge-dev.desktop";
+        "application/x-extension-htm"   = "microsoft-edge-dev.desktop";
+        "application/x-extension-shtml" = "microsoft-edge-dev.desktop";
+        "application/x-extension-xht"   = "microsoft-edge-dev.desktop";
+        "application/x-extension-xhtml" = "microsoft-edge-dev.desktop";
+        "application/xhtml+xml"         = "microsoft-edge-dev.desktop";
+        "text/html"                     = "microsoft-edge-dev.desktop";
+        "x-scheme-handler/chrome"       = "microsoft-edge-dev.desktop";
+        "x-scheme-handler/http"         = "microsoft-edge-dev.desktop";
+        "x-scheme-handler/https"        = "microsoft-edge-dev.desktop";
+        "x-scheme-handler/mailto"       = "microsoft-edge-dev.desktop";
 
         "text/plain"  = ["emacsclient.desktop" "nvim.desktop"];
         "text/x-lisp" = ["emacsclient.desktop" "nvim.desktop"];
