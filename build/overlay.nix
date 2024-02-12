8162245412e3a3ba96d30ee0d4d761281cb4ca38
Sukant Hajra <rrudbskr5g@snkmail.com>
2024-02-11 23:22:58 -0600

    Prebuilt check script

--- build/overlay.nix
+++ build/overlay.nix
@@ -48,6 +48,39 @@ let
 
     pkgs.lists = deepDrvSetToList pkgs.sets;
 
+    checkPrebuilt =
+        let
+            sets = build.pkgs.nixpkgs.prebuilt
+                // build.pkgs.haskell-nix.prebuilt
+                // build.pkgs.shajra.prebuilt;
+            found = builtins.attrValues (deepMerge (lib.collect isDrvSet sets));
+            bashOutPaths = lib.concatMapStringsSep " " (d: d.outPath) found;
+        in prev.writeShellApplication {
+            name = "check-prebuilt";
+            runtimeInputs = [
+                prev.coreutils
+                prev.curl
+            ];
+            text = ''
+                set -eu
+                set -o pipefail
+                OUT_PATHS=(${bashOutPaths})
+                EXIT_CODE=0
+                for path in "''${OUT_PATHS[@]}"
+                do
+                    PATH_PREFIX="''${path%%-*}"
+                    HASH="$(basename "$PATH_PREFIX")"
+                    if ! curl --fail --silent --head \
+                        "https://cache.nixos.org/$HASH.narinfo" >/dev/null
+                    then
+                        echo "NOT FOUND: $path"
+                        EXIT_CODE=1
+                    fi
+                done
+                exit "$EXIT_CODE"
+            '';
+        };
+
     ci.prebuilt.nixpkgs     = joinForCi "prebuilt-nixpkgs" build.pkgs.nixpkgs.prebuilt;
     ci.prebuilt.haskell-nix = joinForCi "prebuilt-haskellnix" build.pkgs.haskell-nix.prebuilt;
     ci.prebuilt.shajra      = joinForCi "prebuilt-shajra" build.pkgs.shajra.prebuilt;
@@ -55,6 +88,7 @@ let
     ci.build.haskell-nix    = joinForCi "build-haskellnix" build.pkgs.haskell-nix.build;
     ci.build.shajra         = joinForCi "build-shajra" build.pkgs.shajra.build;
     ci.all                  = joinForCi "all" build.pkgs;
+    ci.check-prebuilt = checkPrebuilt;
 
 in {
     shajra-provision = {
