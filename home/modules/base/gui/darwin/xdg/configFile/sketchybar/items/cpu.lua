ab020d73918f927fff20419941f04cc6fb0c0e26
Sukant Hajra <rrudbskr5g@snkmail.com>
2024-02-23 11:19:48 -0600

    Darwin: robust display sessions

--- home/modules/base/gui/darwin/xdg/configFile/sketchybar/items/cpu.lua
+++ home/modules/base/gui/darwin/xdg/configFile/sketchybar/items/cpu.lua
@@ -6,15 +6,11 @@ local graph_y_offset = 5
 
 sbar.add("graph", "cpu.user", graph_width, {
     position = "right",
-    width = 0,  -- subsequent items will overlap
-    graph = {
-        color = colors.blue,
-        fill_color = colors.blue,
-        line_width = 1,
-    },
+    width = 0, -- subsequent items will overlap
+    graph = {color = colors.blue, fill_color = colors.blue, line_width = 1},
     y_offset = graph_y_offset,
     icon = {drawing = false},
-    label = {drawing = false},
+    label = {drawing = false}
 })
 
 sbar.add("graph", "cpu.sys", graph_width, {
@@ -23,16 +19,16 @@ sbar.add("graph", "cpu.sys", graph_width, {
     graph = {
         color = colors.red,
         fill_color = colors.transparent(colors.red),
-        line_width = 1,
+        line_width = 1
     },
     y_offset = graph_y_offset,
     icon = {drawing = false},
-    label = {drawing = false},
+    label = {drawing = false}
 })
 
 sbar.add("item", "cpu.percent", {
     position = "right",
-    y_offset=2,
+    y_offset = 2,
     icon = icons.cpu,
     label = "CPU%",
     update_freq = 4,
@@ -43,4 +39,5 @@ sbar.add("item", "cpu.top", {
     position = "right",
     padding_right = 15,
     icon = {drawing = false},
-    label = ""})
+    label = ""
+})
