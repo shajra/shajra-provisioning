fa3bb5f36e6105bdee30987be3e9bac9d48eb76c
Sukant Hajra <dev.sukant@hajra.xyz>
2024-03-05 01:27:12 -0600

    Linux: Qutebrowser: Custom keybindings

--- home/modules/base/gui/linux/programs/qutebrowser/default.nix
+++ home/modules/base/gui/linux/programs/qutebrowser/default.nix
@@ -4,12 +4,28 @@ lib:
     enable = true;
     keyBindings = {
         normal = {
-            ",v" = "hint links spawn --detach mpv {hint-url}";
-            ",p" = "spawn --userscript qute-pass";
-            "<F1>" = lib.mkMerge [
-                "config-cycle tabs.show never always"
+            ";v" = "hint links spawn --detach mpv {hint-url}";
+            ",v" = "spawn --detach mpv {url}";
+            ",p" = "spawn --userscript qute-pass --unfiltered"
+                   + " --username-target secret"
+                   + " --username-pattern \"user: (.+)\"";
+            ",P" = "spawn --userscript qute-pass --unfiltered"
+                   + " --password-only";
+            "gj" = "tab-move +";
+            "gk" = "tab-move -";
+            "<Ctrl-j>" = "tab-move +";
+            "<Ctrl-k>" = "tab-move -";
+            "<Ctrl-=>" = "zoom-in";
+            "<Ctrl-->" = "zoom-out";
+            ">" = "config-cycle tabs.width 15% 20% 25% 30% 35% 40% 45% 50%";
+            "<" = "config-cycle tabs.width 50% 45% 40% 35% 30% 25% 20% 15%";
+            "tt" = lib.mkMerge [
+                "config-cycle tabs.show      never   always"
                 "config-cycle statusbar.show in-mode always"
-                "config-cycle scrolling.bar never always"
+                "config-cycle scrolling.bar  never   always"
+            ];
+            "tT" = lib.mkMerge [
+                "config-cycle tabs.position  left   top"
             ];
         };
     };
