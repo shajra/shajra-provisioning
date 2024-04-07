6e72e7bf66b5bb90a084ca57b105349868a09cd5
Sukant Hajra <dev.sukant@hajra.xyz>
2024-04-06 21:49:31 -0700

    Jujutsu: better defaults and aliases

--- home/modules/base/tui/all/programs/jujutsu/default.nix
+++ home/modules/base/tui/all/programs/jujutsu/default.nix
@@ -5,6 +5,12 @@
             name = "Sukant Hajra";
             email = "dev.sukant@hajra.xyz";
         };
-        ui.default-command = "log";
+        ui.default-command = "l";
+        revset-aliases = {
+            "interesting()" = "ancestors(mine() ~ ::trunk(), 2) | trunk() | @";
+        };
+        aliases = {
+            l = ["log" "-r" "interesting()"];
+        };
     };
 }
--- home/modules/work/default.nix
+++ home/modules/work/default.nix
@@ -3,5 +3,21 @@
 let workEmail = lib.mkForce "shajra@groq.com";
 in {
     programs.git.userEmail = workEmail;
-    programs.jujutsu.settings.user.email = workEmail;
+    programs.jujutsu.settings = {
+        user.email = workEmail;
+        revset-aliases = {
+            "trunk()" = ''
+                latest(
+                    remote_branches(exact:"head",   exact:"origin")   |
+                    remote_branches(exact:"main",   exact:"origin")   |
+                    remote_branches(exact:"master", exact:"origin")   |
+                    remote_branches(exact:"trunk",  exact:"origin")   |
+                    remote_branches(exact:"main",   exact:"upstream") |
+                    remote_branches(exact:"master", exact:"upstream") |
+                    remote_branches(exact:"trunk",  exact:"upstream") |
+                    root()
+                )
+            '';
+        };
+    };
 }
