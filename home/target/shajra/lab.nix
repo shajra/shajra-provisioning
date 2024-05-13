86e803a83e533afc45482c3fa03f2dc06c692ce4
Sukant Hajra <dev.sukant@hajra.xyz>
2024-05-13 12:00:12 -0500

    Fish: Fix lab machine configuration

--- home/target/shajra/lab.nix
+++ home/target/shajra/lab.nix
@@ -30,7 +30,7 @@ in
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
-    programs.fish = import programs/fish;
+    programs.fish = import programs/fish config;
     programs.git = import programs/git lib;
 
     targets.genericLinux.enable = true;
