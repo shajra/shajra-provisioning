b9c57995e926ac90acd90a67834de9d1d35bce13
Sukant Hajra <dev.sukant@hajra.xyz>
2024-06-02 23:56:09 -0500

    NIXOS-REBUILD: broaden impurity to systemd

--- build/nixpkgs/packages/shajra-nixos-rebuild/default.nix
+++ build/nixpkgs/packages/shajra-nixos-rebuild/default.nix
@@ -35,6 +35,8 @@ NIX_EXE="$(command -v /run/current-system/sw/bin/nix \
     ||     true)"
 NIXOS_EXE="$(command -v /run/current-system/sw/bin/nixos-rebuild \
     ||       true)"
+SYSTEMD_RUN_EXE="$(command -v /run/current-system/sw/bin/systemd-run \
+    ||       true)"
 ARGS=()
 
 
@@ -59,6 +61,8 @@ OPTIONS:
                              (default autodetected by hostname)
     -N --nix PATH            filepath of 'nix' executable to use
     -R --nixos-rebuild PATH  filepath of 'nixos-rebuild'
+                             executable to use
+    -S --systemd-run PATH    filepath of 'systemd-run'
                              executable to use
 
     '${progName}' pins all dependencies except for Nix itself,
@@ -107,6 +111,13 @@ main()
             NIXOS_EXE="''${2:-}"
             shift
             ;;
+        -S|--systemd-run)
+            if [ -z "''${2:-}" ]
+            then die "$1 requires argument"
+            fi
+            SYSTEMD_RUN_EXE="''${2:-}"
+            shift
+            ;;
         --)
             HELP_RESPONSE=false
             ;;
@@ -124,9 +135,13 @@ main()
 
 rebuild()
 {
+    PATH="$(path_for "$SYSTEMD_RUN_EXE"):$PATH"
+    PATH="$(path_for "$NIXOS_EXE"):$PATH"
+    PATH="$(path_for "$NIX_EXE"):$PATH"
     /usr/bin/env -i \
+        LOCALE_ARCHIVE="$LOCALE_ARCHIVE" \
         MANPATH=/run/current-system/sw/share/man \
-        PATH="$(path_for "$NIX_EXE"):$(path_for "$NIXOS_EXE"):$PATH" \
+        PATH="$PATH" \
         TERM="$TERM" \
         TERMINFO="''${TERMINFO:-}" \
         nixos-rebuild --flake "$FLAKE" "$@"
