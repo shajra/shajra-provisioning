e7cb1d34859ce8c7f81aaed1b5db257ca2b78e9f
Sukant Hajra <dev.sukant@hajra.xyz>
2024-08-02 22:19:05 -0500

    HOME: BASE: TUI: Simplify Jujutsu config for Darwin
    The complexity has been addressed by upstream Home Manager.

--- home/modules/base/tui/all/home/file/default.nix
+++ home/modules/base/tui/all/home/file/default.nix
@@ -1,22 +1,8 @@
 config: pkgs:
 
-let
-
-    fish  = "${config.programs.fish.package}/bin/fish";
-    makeLink = config.lib.file.mkOutOfStoreSymlink;
-    darwinFiles =  {
-        "Library/Application Support/jj".source =
-            makeLink "${config.xdg.configHome}/jj";
-    };
-    darwinConfig =
-        if pkgs.stdenv.hostPlatform.isDarwin
-        then darwinFiles
-        else {};
-
-in
-
-{
+let fish  = "${config.programs.fish.package}/bin/fish";
+in {
     ".kshrc".text = import ksh/kshrc.nix fish;
     ".nix-channels".source = ./nix-channels;
     ".haskeline".text = "editMode: Vi";
-} // darwinConfig
+}
