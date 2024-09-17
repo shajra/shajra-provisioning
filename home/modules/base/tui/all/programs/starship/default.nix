e1402365e82470922da6d67b80cad40db0461582
Sukant Hajra <dev.sukant@hajra.xyz>
2024-09-16 21:20:58 -0500

    BASE: TUI: ALL: Starship prompt for Jujutsu

--- home/modules/base/tui/all/programs/starship/default.nix
+++ home/modules/base/tui/all/programs/starship/default.nix
@@ -2,7 +2,18 @@
     enable = true;
     settings = {
         cmd_duration.show_notifications = false;
-        status.disabled = false;
-        status.format = "[$symbol $status]($style)";
+        status = {
+            disabled = false;
+            format = "[$symbol $status]($style)";
+        };
+        custom.jj_branch = {
+            command = "jj branch list -T 'if(!remote, name ++ \" \")'";
+            require_repo = true;
+            when = "test -d .jj";
+            description = "Show JJ branches";
+            format = "on [$symbol$output]($style) ";
+            style = "bold purple";
+            symbol = " ";
+        };
     };
 }
