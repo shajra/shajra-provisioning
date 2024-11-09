5495aa47a679aae0d064849147ed4f9810b24cdb
Sukant Hajra <dev.sukant@hajra.xyz>
2024-11-08 20:53:09 -0600

    Update dependencies and documentation

--- build/nixpkgs/overlays/sketchybar/default.nix
+++ build/nixpkgs/overlays/sketchybar/default.nix
@@ -4,5 +4,8 @@ self: super:
     sketchybar = super.sketchybar.overrideAttrs (old: {
         src = self.sources.sketchybar;
         version = "head";
+
+        # DESIGN: versionCheckHook fails to match --version to head
+        nativeInstallCheckInputs = [];
     });
 }
