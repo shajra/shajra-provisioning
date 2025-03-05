45ca30a9c8a2a6c4c360b431e31c3c13694dbc42
Sukant Hajra <dev.sukant@hajra.xyz>
2025-03-05 02:03:53 -0600

    HOME: BAGEL: Clear out machine-specific Fish config

--- home/target/bagel/programs/fish/default.nix
+++ home/target/bagel/programs/fish/default.nix
@@ -1,31 +1,6 @@
+# DESIGN: leaving here for the day it's useful at work
 pkgs:
 
-let sft-tmux-script = pkgs.writeText "sft-tmux.scpt" ''
-        set userName to system attribute "USER"
-        tell application "iTerm"
-            activate
-            set newWindow to (create window with default profile)
-            delay 0.2
-            tell current session of newWindow
-                write text "sft ssh " & userName & ¬
-                    " --command \"tmux -CC new -A -s tmssh\""
-            end tell
-        end tell
-    '';
-
-in {
-    functions = {
-        sft-tunnel = {
-            description = "Tunnel using ScaleFT";
-            body = ''
-                sft ssh --local-port-forward $argv[1]:localhost:$argv[1] (whoami)
-            '';
-        };
-    };
-
     shellAliases = {
-        tunnel-grafana = ''sft-tunnel 4000'';
-        tunnel-nike    = ''sft-tunnel 8428'';
-        tmux-cc = ''osascript ${sft-tmux-script}'';
     };
 }
