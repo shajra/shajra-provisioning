4eb54ed73f3b8d969cf331b8158d00164cc986ef
Sukant Hajra <dev.sukant@hajra.xyz>
2025-04-12 22:37:09 -0500

    BASE: GUI: ALL: More VSCode extensions

--- home/modules/base/gui/all/programs/vscode/default.nix
+++ home/modules/base/gui/all/programs/vscode/default.nix
@@ -9,8 +9,13 @@ in {
         extensions = [
             pkgs.vscode-marketplace.bbenoist.nix
             pkgs.vscode-marketplace.bodil.file-browser
+            pkgs.vscode-marketplace.bradlc.vscode-tailwindcss
             pkgs.vscode-marketplace.elagil.pre-commit-helper
+            pkgs.vscode-marketplace.golang.go
+            pkgs.vscode-marketplace.google.geminicodeassist
+            pkgs.vscode-marketplace.googlecloudtools.cloudcode
             pkgs.vscode-marketplace.gruntfuggly.todo-tree
+            pkgs.vscode-marketplace.hashicorp.terraform
             pkgs.vscode-marketplace.haskell.haskell
             pkgs.vscode-marketplace.jacobdufault.fuzzy-search
             pkgs.vscode-marketplace.jdinhlife.gruvbox
@@ -18,9 +23,18 @@ in {
             pkgs.vscode-marketplace.justusadam.language-haskell
             pkgs.vscode-marketplace.kahole.magit
             pkgs.vscode-marketplace.mkhl.direnv
-            pkgs.vscode-marketplace.ms-vscode.remote-explorer
+            pkgs.vscode-marketplace.ms-azuretools.vscode-docker
+            pkgs.vscode-marketplace.ms-kubernetes-tools.vscode-kubernetes-tools
+            pkgs.vscode-marketplace.ms-python.debugpy
+            pkgs.vscode-marketplace.ms-python.python
+            pkgs.vscode-marketplace.ms-python.vscode-pylance
+            pkgs.vscode-marketplace.ms-vscode-remote.remote-containers
             pkgs.vscode-marketplace.ms-vscode-remote.remote-ssh
             pkgs.vscode-marketplace.ms-vscode.makefile-tools
+            pkgs.vscode-marketplace.ms-vscode.remote-explorer
+            pkgs.vscode-marketplace.ocamllabs.ocaml-platform
+            pkgs.vscode-marketplace.redhat.vscode-xml
+            pkgs.vscode-marketplace.redhat.vscode-yaml
             pkgs.vscode-marketplace.rust-lang.rust-analyzer
             pkgs.vscode-marketplace.tamasfe.even-better-toml
             pkgs.vscode-marketplace.timonwong.shellcheck
