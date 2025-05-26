6e8abcd5786b50a8a9a548271e992e3f17a19a55
Sukant Hajra <dev.sukant@hajra.xyz>
2025-05-25 19:11:47 -0500

    HOME: BASE: GUI: LINUX: Firefox add-ons

--- home/modules/base/gui/linux/programs/firefox/default.nix
+++ home/modules/base/gui/linux/programs/firefox/default.nix
@@ -11,32 +11,11 @@ in
     profiles.default = {
         isDefault = true;
         extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-            # DESIGN: Don't feel these are useful any more
-            #duckduckgo-privacy-essentials
-            #simple-tab-groups
-            #stylus
-            #tree-style-tab
+            auto-tab-discard
+            feedbroreader
             grammarly
             tridactyl
         ];
-        /*
-        # DESIGN: https://github.com/piroor/treestyletab/wiki/Code-snippets-for-custom-style-rules
-        userChrome = ''
-            #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
-              opacity: 0;
-              pointer-events: none;
-            }
-            #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
-                visibility: collapse !important;
-            }
-            #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar .titlebar-spacer {
-                border-inline-end: none;
-            }
-            #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
-                display: none;
-            }
-        '';
-        */
         settings = {
             "browser.download.dir" = "${home}/tmp/download";
             "browser.startup.page" = 3;
@@ -50,8 +29,6 @@ in
             "services.sync.engine.tabs" = true;
             "sidebar.revamp" = true;
             "sidebar.verticalTabs" = true;
-            # DESIGN: https://github.com/piroor/treestyletab/wiki/Code-snippets-for-custom-style-rules
-            #"toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         };
     };
 }
