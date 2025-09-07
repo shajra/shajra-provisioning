6b29f3f7ee4addb6d4ebcd453aa1b1bd2f09342a
Sukant Hajra <dev.sukant@hajra.xyz>
2025-09-07 00:05:34 -0500

    HOME: BASE: TUI: Review Doom init.el

--- home/modules/base/tui/all/xdg/configFile/doom/init.el
+++ home/modules/base/tui/all/xdg/configFile/doom/init.el
@@ -1,6 +1,7 @@
 ;;; -*- lexical-binding: t; -*-
 
 
+;; REVISIT: 25-09-07: Later check for new modules to consider
 (doom!
 
   :completion
@@ -17,10 +18,14 @@
   ;;(ligatures        ; ligatures and symbols to make your code pretty again
   ;;  +extra          ;     consider removing if performance impacted
   ;;)
+  ;;minimap           ; show a map of the code on the side
   modeline            ; snazzy, Atom-inspired modeline, plus API
   nav-flash           ; blink cursor line after big motions
+  ;;neotree           ; a project drawer, like NERDTree for vim
   ophints             ; highlight the region an operation acts on
   (popup +defaults)   ; tame sudden yet inevitable temporary windows
+  ;;smooth-scroll     ; so smooth you won't believe it's not butter
+  ;;tabs              ; a tab bar for Emacs
   (treemacs +lsp)     ; a project drawer, like neotree but cooler
   unicode             ; extended unicode support for various languages
   (vc-gutter          ; vcs diff in the fringe
@@ -37,7 +42,11 @@
   file-templates      ; auto-snippets for empty files
   fold                ; (nigh) universal code folding
   format              ; automated prettiness
+  ;;god               ; run Emacs commands without modifier keys
+  ;;lispy             ; vim for lisp, for people who don't like vim
   multiple-cursors    ; editing in many places at once
+  ;;objed             ; text object editing for the innocent
+  ;;parinfer          ; turn lisp into python, sort of
   rotate-text         ; cycle region at point between text candidates
   snippets            ; my elves. They type so I don't have to
   word-wrap           ; soft wrapping with language-aware indent
@@ -46,6 +55,7 @@
   (dired              ; making dired pretty [functional]
    +icons)            ;     icons are nice
   electric            ; smarter, keyword-based electric-indent
+  ;;eww               ; the internet is gross
   (ibuffer +icons)    ; interactive buffer management
   undo                ; persistent, smarter undo for your inevitable mistakes
   vc                  ; version-control and Emacs, sitting in a tree
