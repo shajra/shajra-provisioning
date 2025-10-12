b3d5063d8700a69f2866ce248d247949ed0aeeb3
Sukant Hajra <dev.sukant@hajra.xyz>
2025-10-11 23:55:53 -0500

    HOME: CENTRALIZED: rclone

--- home/modules/centralized/default.nix
+++ home/modules/centralized/default.nix
@@ -4,6 +4,20 @@
   imports = [ ../ubiquity ];
   home.extraPackages = build.pkgs.lists.centralized;
   programs.newsboat.enable = true;
+  programs.rclone.enable = true;
+  programs.rclone.remotes = {
+    google-tnks-public-drive = {
+      config = {
+        type = "drive";
+        scope = "drive";
+      };
+      secrets = {
+        client_id = "/run/secrets/rclone/google-tnks-public-drive/client_id";
+        client_secret = "/run/secrets/rclone/google-tnks-public-drive/client_secret";
+        token = "/run/secrets/rclone/google-tnks-public-drive/token";
+      };
+    };
+  };
   programs.rofi.pass.enable = true;
   programs.rofi.pass.stores = [ "${config.home.homeDirectory}/src/live/password-store" ];
 }
