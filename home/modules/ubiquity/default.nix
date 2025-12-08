a2e2eaa3ae3c8285e502c714aeacc1a5314768d3
Sukant Hajra <dev.sukant@hajra.xyz>
2025-12-08 10:53:05 -0600

    Downgrade Home Mananger stateVersion

--- home/modules/ubiquity/default.nix
+++ home/modules/ubiquity/default.nix
@@ -5,6 +5,10 @@
     ./packages
     ./theme
   ];
-  home.stateVersion = "25.11";
+
+  # REVISIT: Holding back while churn from targets.darwin.{copyApps,linkApps}
+  # settles downs.
+  home.stateVersion = "25.05";
+
   nix.package = build.infra.np.nixpkgs.system.nix;
 }
