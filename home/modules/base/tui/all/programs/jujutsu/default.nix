accc9969941ad13c5ebe678a0f062ef1fe296797
Sukant Hajra <dev.sukant@hajra.xyz>
2025-12-09 09:50:57 -0600

    HOME: BASE: TUI: Jututsu config change for new version

--- home/modules/base/tui/all/programs/jujutsu/default.nix
+++ home/modules/base/tui/all/programs/jujutsu/default.nix
@@ -8,7 +8,7 @@
     ui.default-command = "l";
     revset-aliases = {
       "interesting" = "interesting(2)";
-      "interesting(last_n)" = "ancestors(author(\"Sukant Hajra\") ~ ::trunk(), last_n) | trunk() | @";
+      "interesting(last_n)" = "ancestors(author(substring:\"Sukant Hajra\") ~ ::trunk(), last_n) | trunk() | @";
     };
     aliases = {
       l = [
