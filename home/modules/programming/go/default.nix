ec390668a3df62bc764ab8bc7fe0f786bc6516c1
Sukant Hajra <dev.sukant@hajra.xyz>
2026-05-10 19:36:39 -0500

    HOME: PROG: GO: Environment setting

--- home/modules/programming/go/default.nix
+++ home/modules/programming/go/default.nix
@@ -1,6 +1,17 @@
-{ build, ... }:
+{ build, config, ... }:
 
 {
   imports = [ ../../ubiquity ];
   home.extraPackages = build.pkgs.lists.programming.go;
+
+  programs.go = {
+    enable = true;
+    package = null;
+    env = {
+      GOPATH = "${config.xdg.dataHome}/go";
+      GOMODCACHE = "${config.xdg.cacheHome}/go/pkg/mod";
+      GOCACHE = "${config.xdg.cacheHome}/go-build";
+      GOBIN = "${config.home.homeDirectory}/.local/bin";
+    };
+  };
 }
