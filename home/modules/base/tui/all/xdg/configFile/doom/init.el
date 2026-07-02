571fd6a7f36d45a3719e2198187a184e0216d510
Sukant Hajra <dev.sukant@hajra.xyz>
2026-07-01 23:54:35 -0500

    HOME: BASE: TUI: ALL: Doom deprecation

--- home/modules/base/tui/all/xdg/configFile/doom/init.el
+++ home/modules/base/tui/all/xdg/configFile/doom/init.el
@@ -16,8 +16,8 @@
   )
 
   :ui
+  dashboard           ; a nifty splash screen for Emacs
   doom                ; what makes DOOM look the way it does
-  doom-dashboard      ; a nifty splash screen for Emacs
   ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
   (emoji +unicode)    ; 🙂
   hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
