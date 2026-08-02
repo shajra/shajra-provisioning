c4650776bbb258b0790a49d6c8c1befbc3997b75
Sukant Hajra <dev.sukant@hajra.xyz>
2026-08-01 23:33:06 -0500

    HOME: BASE: GUI: LINUX: Flameshot workaround

--- home/modules/base/gui/linux/services/flameshot/default.nix
+++ home/modules/base/gui/linux/services/flameshot/default.nix
@@ -9,6 +9,10 @@ config:
       savePath = "${config.home.homeDirectory}/tmp/screenshots";
       savePathFixed = false;
       startupLaunch = false;
+
+      # REVISIT: 2026-08-01: BLOCKED: Workaround below
+      # See https://github.com/nix-community/home-manager/issues/9201
+      useX11LegacyScreenshot = true;
     };
   };
 }
